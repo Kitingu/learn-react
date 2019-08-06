79ec77efea0b7e0ce4c4d8e2266a9030fa1989fb
benlegendj <benlegendj@gmail.com>
2019-08-06 23:25:20 +0300

    build it challenge 1

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,97 +1,38 @@
 "use strict";
 
-/**
- * react code live here
- * jsx javascript XML
- * undefined null and false are ignored by jsx
- * ternary operator, unlike normal functions can be used inside jsx expression
- */
+var visibility = false;
 
-//assignment 2
-var app = {
-  title: "Indecision App",
-  subtitle: "Talk is cheap",
-  options: []
+var toggleVisibility = function toggleVisibility() {
+  visibility = !visibility;
+  render();
 };
 
-var onFormSubmit = function onFormSubmit(e) {
-  e.preventDefault();
-  var option = e.target.elements.option.value;
-  if (option) {
-    console.log(option);
-    app.options.push(option);
-    e.target.elements.option.value = "";
-    renderTemplate();
-  }
-};
-var numbers = [55, 158, 82];
-var resetLength = function resetLength(e) {
-  app.options.length = 0;
-  renderTemplate();
-};
-
-var onMakeDecision = function onMakeDecision() {
-  var randomNum = Math.floor(Math.random() * app.options.length);
-  var option = app.options[randomNum];
-  alert(option);
-};
-
-var renderTemplate = function renderTemplate() {
-  var template = React.createElement(
+var render = function render() {
+  var jsx = React.createElement(
     "div",
     null,
     React.createElement(
       "h1",
       null,
-      app.title
-    ),
-    app.title && React.createElement(
-      "p",
-      null,
-      app.subtitle
-    ),
-    React.createElement(
-      "p",
-      null,
-      app.options.length > 0 ? "Here are your options" : "No options"
+      "Visibility Toggle"
     ),
     React.createElement(
       "button",
-      { disabled: app.options.length == 0, onClick: onMakeDecision },
-      " What should I do"
+      { onClick: toggleVisibility },
+      visibility ? "Hide details" : "Show details"
     ),
-    React.createElement(
-      "button",
-      { onClick: resetLength },
-      "remove all"
-    ),
-    React.createElement(
-      "ol",
+    visibility && React.createElement(
+      "div",
       null,
-      app.options.map(function (option) {
-        return React.createElement(
-          "li",
-          { key: option },
-          option
-        );
-      })
-    ),
-    React.createElement(
-      "form",
-      { onSubmit: onFormSubmit },
-      React.createElement("input", { type: "text", name: "option" }),
       React.createElement(
-        "button",
+        "p",
         null,
-        "Add option"
+        "Hey. These are some details you can now see!"
       )
     )
   );
-  ReactDOM.render(template, appRoot);
+
+  ReactDOM.render(jsx, document.getElementById("app"));
 };
 
-// functions in react expressions are referenced to not called
-// arrays in jsx must have a key
-// react ignores null undefined and false i.e falsie statements and objects, booleans
-var appRoot = document.getElementById("app");
-renderTemplate();
+render();
