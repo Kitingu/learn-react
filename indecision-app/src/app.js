be6cbe938b64da56438b0d5105335a2d03b83dda
benlegendj <benlegendj@gmail.com>
2019-08-16 23:33:15 +0300

    intro to new class syntax and transform class plugin

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -3,3 +3,20 @@ import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+
+class OldSyntax {
+  constructor() {
+    this.name = "mike";
+  }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+// new syntax
+
+class NewSyntax {
+  name = "mike";
+}
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
