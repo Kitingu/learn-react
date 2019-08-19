47cfb385cbee3919640f4b229bd08aacf2b7ad5c
benlegendj <benlegendj@gmail.com>
2019-08-19 10:40:08 +0300

    intro to built in props

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -6,17 +6,18 @@ const Layout = props => {
   return (
     <div>
       <p>Header</p>
-      {props.content}
+      {props.children}
       <p>Footer</p>
     </div>
   );
 };
 
-const template = (
-  <div>
-    <h1>Title</h1>
-    <p>This is my page</p>
-  </div>
-);
 
-ReactDOM.render(<Layout content={template} />, document.getElementById("app"));
+ReactDOM.render((
+    <Layout>
+        <div>
+            <h1>Title</h1>
+            <p>This is my page</p>
+        </div>
+    </Layout>
+    ), document.getElementById("app"));
