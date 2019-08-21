919fef4c1f27c647f8bda2f31de9b558a15cf5ed
benlegendj <benlegendj@gmail.com>
2019-08-22 00:30:55 +0300

    set up 404 page

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,21 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route,Switch } from "react-router-dom";
 import "./styles/style.scss";
 import "normalize.css/normalize.css";
 
 const ExpenseDashboardPage = () => <div>this is from the dashboard</div>;
-const AddExpenseComponent = () => <div>this is add expense </div>;
-const EditComponent = () => <div>this is edit expense </div>;
-const HelpComponent = () => <div>this is help expense </div>;
+const AddExpenseComponentPage = () => <div>this is add expense </div>;
+const EditComponentPage = () => <div>this is edit expense </div>;
+const HelpComponentPage = () => <div>this is help expense </div>;
+const NotFoundPage = () => <div>404 Page Not Found </div>;
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path='/' component={ExpenseDashboardPage} exact={true} />
-      <Route path='/create' component={AddExpenseComponent} />
-      <Route path='/edit' component={EditComponent} />
-      <Route path='/help' component={HelpComponent} />
-    </div>
+      <Route path='/create' component={AddExpenseComponentPage} />
+      <Route path='/edit' component={EditComponentPage} />
+      <Route path='/help' component={HelpComponentPage} />
+      <Route component={NotFoundPage}/>
+    </Switch>
   </BrowserRouter>
 );
 ReactDOM.render(routes, document.getElementById("app"));
