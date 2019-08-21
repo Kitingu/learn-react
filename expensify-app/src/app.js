eb723304962034993669aed708ca1e7ff84b8ea2
benlegendj <benlegendj@gmail.com>
2019-08-22 00:16:46 +0300

    intro to react router

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import IndecisionApp from "./components/IndecisionApp";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./styles/style.scss";
-import "normalize.css/normalize.css"
-ReactDOM.render(<p>This is just a boilerplate</p>, document.getElementById("app"));
+import "normalize.css/normalize.css";
+
+const ExpenseDashboardPage = () => <div>this is from the dashboard</div>;
+const AddExpenseComponent = () => <div>this is add expense </div>;
+const EditComponent = () => <div>this is edit expense </div>;
+const HelpComponent = () => <div>this is help expense </div>;
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path='/' component={ExpenseDashboardPage} exact={true} />
+      <Route path='/create' component={AddExpenseComponent} />
+      <Route path='/edit' component={EditComponent} />
+      <Route path='/help' component={HelpComponent} />
+    </div>
+  </BrowserRouter>
+);
+ReactDOM.render(routes, document.getElementById("app"));
