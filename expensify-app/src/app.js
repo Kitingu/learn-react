44614dc24f0d15b10975474bcd88f8df6ac06b4c
benlegendj <benlegendj@gmail.com>
2019-08-22 16:24:16 +0300

    linking multiple pages using Link

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route,Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import "./styles/style.scss";
 import "normalize.css/normalize.css";
 
@@ -8,16 +8,33 @@ const ExpenseDashboardPage = () => <div>this is from the dashboard</div>;
 const AddExpenseComponentPage = () => <div>this is add expense </div>;
 const EditComponentPage = () => <div>this is edit expense </div>;
 const HelpComponentPage = () => <div>this is help expense </div>;
-const NotFoundPage = () => <div>404 Page Not Found </div>;
+const NotFoundPage = () => (
+  <div>
+    404 Page Not Found
+    <Link to='/'> Home </Link>
+  </div>
+);
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <Link to='/'> Home </Link>
+    <Link to='/create'> Create </Link>
+    <Link to='/edit'> Edit </Link>
+    <Link to='/help'> Help </Link>
+  </header>
+);
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path='/' component={ExpenseDashboardPage} exact={true} />
-      <Route path='/create' component={AddExpenseComponentPage} />
-      <Route path='/edit' component={EditComponentPage} />
-      <Route path='/help' component={HelpComponentPage} />
-      <Route component={NotFoundPage}/>
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path='/' component={ExpenseDashboardPage} exact={true} />
+        <Route path='/create' component={AddExpenseComponentPage} />
+        <Route path='/edit' component={EditComponentPage} />
+        <Route path='/help' component={HelpComponentPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 ReactDOM.render(routes, document.getElementById("app"));
