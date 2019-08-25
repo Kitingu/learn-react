df221e6862405c336e5ce1082a5218c3e8c28e83
benlegendj <benlegendj@gmail.com>
2019-08-25 04:32:36 +0300

    array destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,3 +1,4 @@
+// object destructuring
 const person = {
   name: "ben",
   age: 26,
@@ -12,3 +13,14 @@ const { age, name = "anonymous" } = person;
 const { city, temp: temperature } = person.location;
 // temp:temperature  --> rename destructured variable
 console.log(`${age} ${name} ${temperature}`);
+
+// array destructuring
+
+const address = ["19665", "allsops", "kenya"];
+
+const [addressNumber, cityName, state] = address;
+console.log(`you are in ${addressNumber} ${cityName} ${state}`);
+// array destructuring asigns variable name and values to items inside an array according to their indexes
+// if you want to destructure specific values, you use commasto separate the unwanted values and the ones you want
+// eg const [, , state] = address , only destructures the state from the given array
+// to set default, const [, , state="kenya"] = address this sets the value to the default even when the value is not provided or maybe the array is empty.
