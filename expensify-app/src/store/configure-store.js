fb2e3e0278cc1084f45a86bed6a6ee92531c27e1
benlegendj <benlegendj@gmail.com>
2019-09-19 13:26:31 +0300

    configure redux dev tools

--- expensify-app/src/store/configure-store.js
+++ expensify-app/src/store/configure-store.js
@@ -4,11 +4,13 @@ import filtersReducer from "../reducers/filters";
 
 // store creation
 export default () => {
-  const store = createStore(
-    combineReducers({
-      expenses: expensesReducer,
-      filters: filtersReducer
-    })
-  );
-  return store;
+    const store = createStore(
+        combineReducers({
+            expenses: expensesReducer,
+            filters: filtersReducer
+        }),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+    return store;
 };
