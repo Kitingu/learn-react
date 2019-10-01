1a43657b238bc0f69861212e4c728526b50a171e
benlegendj <benlegendj@gmail.com>
2019-10-01 16:30:35 +0300

    remove dummy data

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,32 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import AppRouter from "./routers/AppRouter";
-import configureStore from "./store/configure-store";
-import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
-import "./styles/style.scss";
-import "normalize.css/normalize.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configure-store';
+import getVisibleExpenses from './selectors/expenses';
+import './styles/style.scss';
+import 'normalize.css/normalize.css';
 
 const store = configureStore();
-
-store.dispatch(
-    addExpense({ description: "water bill", amount: 70000, createdAt: 112 })
-);
-store.dispatch(
-DMMHEHE Fellow    addExpense({ description: "gas-bill", amount: 200, createdAt: 132 })
-);
-store.dispatch(
-  addExpense({ description: "rent", amount: 50000, createdAt: 12 })
-);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
+	<Provider store={store}>
+		<AppRouter />
+	</Provider>
 );
-ReactDOM.render(jsx, document.getElementById("app"));
+ReactDOM.render(jsx, document.getElementById('app'));
