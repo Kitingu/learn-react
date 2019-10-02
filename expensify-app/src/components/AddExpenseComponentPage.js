3763f1297ca000c1cfe53a31b6e18a49ab95c747
benlegendj <benlegendj@gmail.com>
2019-10-03 00:51:56 +0300

    test add expense page

--- expensify-app/src/components/AddExpenseComponentPage.js
+++ expensify-app/src/components/AddExpenseComponentPage.js
@@ -1,17 +1,27 @@
-import React from "react";
-import { connect } from "react-redux";
-import ExpenseForm from "./ExpenseForm";
-import { addExpense } from "../actions/expenses";
-const AddExpenseComponentPage = (props) => (
-    <div>
-        <h1>Add expense</h1>
-        <ExpenseForm
-            onSubmit={(expense) => {
-                props.dispatch(addExpense(expense));
-                props.history.push('/')
-            }}
-        />
-    </div>
-);
+import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
+export class AddExpenseComponentPage extends React.Component {
+	onSubmit = (expense) => {
+		this.props.addExpense(expense);
+		this.props.history.push('/');
+	};
+	render() {
+		return (
+			<div>
+				<h1>Add expense</h1>
+				<ExpenseForm onSubmit={this.onSubmit} />
+			</div>
+		);
+	}
+}
+const mapDispatchToProps = (dispatch) => {
+	addExpense: (expense) => dispatch(addExpense(expense));
+};
 
-export default connect()(AddExpenseComponentPage);
+// use a dispatch function function to make it easy to test the component
+export default connect(
+	undefined,
+	mapDispatchToProps
+)(AddExpenseComponentPage);
